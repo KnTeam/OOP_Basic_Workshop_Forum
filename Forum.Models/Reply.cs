d5e18fccad3db7700fb48e2b9f05d79747711167
Steffkn <steff.kn@abv.bg>
2018-03-12 16:15:30 +0200

    Added: Forum.Models library Added: Models -> Post, User, Reply, Category

---
+++ Forum.Models/Reply.cs
@@ -0,0 +1,14 @@
+﻿namespace Forum.Models
+{
+    using System.Collections.Generic;
+
+    public class Reply
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public int AuthorId { get; set; }
+        public int PostId { get; set; }
+    }
+}
